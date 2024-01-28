c91f13e0ea7c895e5117b81d3bf9d719a1512a1a
Gabriel Peres Bernes <gabriel.p_b@hotmail.com>
2024-01-29 01:50:57 +0200

    refactor: imports

--- integration_test/get_city_weather_test.dart
+++ integration_test/get_city_weather_test.dart
@@ -1,6 +1,3 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:weather_to_rock/app/components/card_tile.dart';
